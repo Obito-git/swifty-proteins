86835dd47c6190d7cb5f9b4f5ae54b8fdc66e462
amyroshn <amyroshn@student.42.fr>
2024-05-21 22:40:33 +0200

    first

---
+++ README.md
@@ -0,0 +1,8 @@
+# swifty_proteins
+
+swifty_proteins
+
+## Getting Started
+
+https://www.rcsb.org
+https://files.rcsb.org/download/{PDB_ID}.pdb
---
+++ android/app/src/main/kotlin/fr/swifty_proteins42/swifty_proteins/MainActivity.kt
@@ -0,0 +1,5 @@
+package fr.swifty_proteins42.swifty_proteins
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
