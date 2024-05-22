c3dc3be7b4544abc2eba1a24d170e721541dbc45
amyroshn <amyroshn@student.42.fr>
2024-05-22 22:12:48 +0200

    add back rocket

--- back/src/main.rs
+++ back/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/hello/<name>/<age>")]
+fn hello(name: &str, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+#[get("/")]
+fn hello_raw() -> String {
+    format!("Hello world!")
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![hello, hello_raw])
 }
