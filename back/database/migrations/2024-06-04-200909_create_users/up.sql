1e7a9ba5b34f046f9569dddb5f915c43c67dc315
Obito <antonmyroshnychenko@gmail.com>
2024-06-04 23:08:38 +0200

    scripts update

--- back/database/migrations/2024-06-04-200909_create_users/up.sql
+++ back/database/migrations/2024-06-04-200909_create_users/up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users
+CREATE TABLE IF NOT EXISTS users
 (
     id       INTEGER NOT NULL PRIMARY KEY,
     username TEXT    NOT NULL UNIQUE CHECK (LENGTH(username) >= 2 AND LENGTH(username) <= 30),
--- back/src/database/schema.rs
+++ back/src/database/schema.rs
@@ -1,9 +1,20 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    proteins (code) {
+        code -> Text,
+    }
+}
+
+diesel::table! {
     users (id) {
         id -> Integer,
         username -> Text,
         password -> Text,
     }
 }
+
+diesel::allow_tables_to_appear_in_same_query!(
+    proteins,
+    users,
+);
